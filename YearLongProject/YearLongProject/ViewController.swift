707ff826e444c7de53caabd581699167b7c8b496
Derek Stengel <derekstengel@Dereks-MacBook-Pro.local>
2024-03-01 08:45:37 -0700

    added year long project

---
+++ YearLongProject/YearLongProject/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  YearLongProject
+//
+//  Created by Derek Stengel on 2/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var topStackView: UIStackView!
+    @IBOutlet weak var middleStackView: UIStackView!
+    @IBOutlet weak var bottomStackView: UIStackView!
+    
+//    var tsv = topStackView
+//    var msv = middleStackView
+//    var bsv = bottomStackView
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        UIImage.cornerRadius = 0.5
+        // Do any additional setup after loading the view.
+    }
+
+//    topStackView.layer.cornerRadius = 10
+}
+
